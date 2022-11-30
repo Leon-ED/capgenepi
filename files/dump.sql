196013ff1bb40bd9f5e4ba54dbda4abd5c1436a3
Leon-ED <edmeeleon@outlook.fr>
2022-11-30 20:37:18 +0100

    dump

--- files/dump.sql
+++ files/dump.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : localhost:3306
--- Généré le : mer. 30 nov. 2022 à 19:11
+-- Généré le : mer. 30 nov. 2022 à 19:36
 -- Version du serveur : 10.9.4-MariaDB-log
 -- Version de PHP : 8.1.10
 
@@ -24,10 +24,10 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Structure de la table `compte`
+-- Structure de la table `b__compte`
 --
 
-CREATE TABLE `compte` (
+CREATE TABLE `b__compte` (
   `id` int(11) NOT NULL,
   `login` varchar(50) NOT NULL,
   `password` varchar(255) NOT NULL,
@@ -38,10 +38,10 @@ CREATE TABLE `compte` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Déchargement des données de la table `compte`
+-- Déchargement des données de la table `b__compte`
 --
 
-INSERT INTO `compte` (`id`, `login`, `password`, `email`, `nom`, `prenom`, `role`) VALUES
+INSERT INTO `b__compte` (`id`, `login`, `password`, `email`, `nom`, `prenom`, `role`) VALUES
 (1, '2001458436', '$2y$10$7xldYUDD6EVmjAPjg/UzBOYl/cjRSoYckiHP5ji7o0aojp/9EKNJG', 'admin@bank.eu', 'Admin', 'nistrateur', 'ADMIN'),
 (2, '4526452419', '$2y$10$9y16F6Qmt5.uR8MY8k2jNOTOhw77AdyRzL9zWs3Qj1dbBNRyYUGM2', 'PO@cap-gemi.ni', 'Product', 'Owner', 'PO'),
 (3, '8755269857', '$2y$10$mcEyY9CCt0US1mOwrxXFAOCjr8AU7JmLLlaVbZgD3NbBMFQ.PVzm.', 'pastille-vomitive@mc.do', 'Ronald', 'McDonald', 'CLIENT'),
@@ -50,10 +50,10 @@ INSERT INTO `compte` (`id`, `login`, `password`, `email`, `nom`, `prenom`, `role
 -- --------------------------------------------------------
 
 --
--- Structure de la table `controle`
+-- Structure de la table `b__controle`
 --
 
-CREATE TABLE `controle` (
+CREATE TABLE `b__controle` (
   `id` int(11) NOT NULL,
   `SIREN` char(9) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -61,10 +61,10 @@ CREATE TABLE `controle` (
 -- --------------------------------------------------------
 
 --
--- Structure de la table `entreprise`
+-- Structure de la table `b__entreprise`
 --
 
-CREATE TABLE `entreprise` (
+CREATE TABLE `b__entreprise` (
   `SIREN` char(9) NOT NULL,
   `Raison_sociale` char(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -72,10 +72,10 @@ CREATE TABLE `entreprise` (
 -- --------------------------------------------------------
 
 --
--- Structure de la table `impaye`
+-- Structure de la table `b__impaye`
 --
 
-CREATE TABLE `impaye` (
+CREATE TABLE `b__impaye` (
   `numero_dossier_impaye` varchar(50) NOT NULL,
   `code` varchar(2) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -83,10 +83,10 @@ CREATE TABLE `impaye` (
 -- --------------------------------------------------------
 
 --
--- Structure de la table `motifs_impayes`
+-- Structure de la table `b__motifs_impayes`
 --
 
-CREATE TABLE `motifs_impayes` (
+CREATE TABLE `b__motifs_impayes` (
   `code` varchar(2) NOT NULL,
   `libelle` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -94,19 +94,19 @@ CREATE TABLE `motifs_impayes` (
 -- --------------------------------------------------------
 
 --
--- Structure de la table `role`
+-- Structure de la table `b__role`
 --
 
-CREATE TABLE `role` (
+CREATE TABLE `b__role` (
   `role` varchar(50) NOT NULL,
   `libelle` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Déchargement des données de la table `role`
+-- Déchargement des données de la table `b__role`
 --
 
-INSERT INTO `role` (`role`, `libelle`) VALUES
+INSERT INTO `b__role` (`role`, `libelle`) VALUES
 ('ADMIN', 'Administrateur'),
 ('CLIENT', 'Client'),
 ('PO', 'Product Owner');
@@ -114,10 +114,10 @@ INSERT INTO `role` (`role`, `libelle`) VALUES
 -- --------------------------------------------------------
 
 --
--- Structure de la table `transaction`
+-- Structure de la table `b__transaction`
 --
 
-CREATE TABLE `transaction` (
+CREATE TABLE `b__transaction` (
   `numero_transaction` int(11) NOT NULL,
   `devise` char(3) NOT NULL,
   `montant` int(11) NOT NULL,
@@ -133,52 +133,52 @@ CREATE TABLE `transaction` (
 --
 
 --
--- Index pour la table `compte`
+-- Index pour la table `b__compte`
 --
-ALTER TABLE `compte`
+ALTER TABLE `b__compte`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `login` (`login`),
   ADD UNIQUE KEY `email` (`email`),
   ADD KEY `role` (`role`);
 
 --
--- Index pour la table `controle`
+-- Index pour la table `b__controle`
 --
-ALTER TABLE `controle`
+ALTER TABLE `b__controle`
   ADD PRIMARY KEY (`id`,`SIREN`),
   ADD KEY `SIREN` (`SIREN`);
 
 --
--- Index pour la table `entreprise`
+-- Index pour la table `b__entreprise`
 --
-ALTER TABLE `entreprise`
+ALTER TABLE `b__entreprise`
   ADD PRIMARY KEY (`SIREN`);
 
 --
--- Index pour la table `impaye`
+-- Index pour la table `b__impaye`
 --
-ALTER TABLE `impaye`
+ALTER TABLE `b__impaye`
   ADD PRIMARY KEY (`numero_dossier_impaye`),
   ADD KEY `code` (`code`);
 
 --
--- Index pour la table `motifs_impayes`
+-- Index pour la table `b__motifs_impayes`
 --
-ALTER TABLE `motifs_impayes`
+ALTER TABLE `b__motifs_impayes`
   ADD PRIMARY KEY (`code`),
   ADD UNIQUE KEY `libelle` (`libelle`);
 
 --
--- Index pour la table `role`
+-- Index pour la table `b__role`
 --
-ALTER TABLE `role`
+ALTER TABLE `b__role`
   ADD PRIMARY KEY (`role`),
   ADD UNIQUE KEY `libelle` (`libelle`);
 
 --
--- Index pour la table `transaction`
+-- Index pour la table `b__transaction`
 --
-ALTER TABLE `transaction`
+ALTER TABLE `b__transaction`
   ADD PRIMARY KEY (`numero_transaction`),
   ADD KEY `numero_dossier_impaye` (`numero_dossier_impaye`),
   ADD KEY `SIREN` (`SIREN`);
@@ -188,9 +188,9 @@ ALTER TABLE `transaction`
 --
 
 --
--- AUTO_INCREMENT pour la table `compte`
+-- AUTO_INCREMENT pour la table `b__compte`
 --
-ALTER TABLE `compte`
+ALTER TABLE `b__compte`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
@@ -198,30 +198,30 @@ ALTER TABLE `compte`
 --
 
 --
--- Contraintes pour la table `compte`
+-- Contraintes pour la table `b__compte`
 --
-ALTER TABLE `compte`
-  ADD CONSTRAINT `compte_ibfk_1` FOREIGN KEY (`role`) REFERENCES `role` (`role`);
+ALTER TABLE `b__compte`
+  ADD CONSTRAINT `b__compte_ibfk_1` FOREIGN KEY (`role`) REFERENCES `b__role` (`role`);
 
 --
--- Contraintes pour la table `controle`
+-- Contraintes pour la table `b__controle`
 --
-ALTER TABLE `controle`
-  ADD CONSTRAINT `controle_ibfk_2` FOREIGN KEY (`SIREN`) REFERENCES `entreprise` (`SIREN`),
-  ADD CONSTRAINT `controle_ibfk_3` FOREIGN KEY (`id`) REFERENCES `compte` (`id`);
+ALTER TABLE `b__controle`
+  ADD CONSTRAINT `b__controle_ibfk_2` FOREIGN KEY (`SIREN`) REFERENCES `b__entreprise` (`SIREN`),
+  ADD CONSTRAINT `b__controle_ibfk_3` FOREIGN KEY (`id`) REFERENCES `b__compte` (`id`);
 
 --
--- Contraintes pour la table `impaye`
+-- Contraintes pour la table `b__impaye`
 --
-ALTER TABLE `impaye`
-  ADD CONSTRAINT `impaye_ibfk_1` FOREIGN KEY (`code`) REFERENCES `motifs_impayes` (`code`);
+ALTER TABLE `b__impaye`
+  ADD CONSTRAINT `b__impaye_ibfk_1` FOREIGN KEY (`code`) REFERENCES `b__motifs_impayes` (`code`);
 
 --
--- Contraintes pour la table `transaction`
+-- Contraintes pour la table `b__transaction`
 --
-ALTER TABLE `transaction`
-  ADD CONSTRAINT `transaction_ibfk_1` FOREIGN KEY (`numero_dossier_impaye`) REFERENCES `impaye` (`numero_dossier_impaye`),
-  ADD CONSTRAINT `transaction_ibfk_2` FOREIGN KEY (`SIREN`) REFERENCES `entreprise` (`SIREN`);
+ALTER TABLE `b__transaction`
+  ADD CONSTRAINT `b__transaction_ibfk_1` FOREIGN KEY (`numero_dossier_impaye`) REFERENCES `b__impaye` (`numero_dossier_impaye`),
+  ADD CONSTRAINT `b__transaction_ibfk_2` FOREIGN KEY (`SIREN`) REFERENCES `b__entreprise` (`SIREN`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
