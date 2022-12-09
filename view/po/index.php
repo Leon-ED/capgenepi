6563ebae59ad718223701456066644d73a97e14e
Leon-ED <edmeeleon@outlook.fr>
2022-12-09 21:38:19 +0100

    graphes

--- view/po/index.php
+++ view/po/index.php
@@ -23,6 +23,7 @@ require_once("../../controller/po_controller.php");
     <script src="../../files/product_owner.js" defer></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js" integrity="sha512-qZvrmS2ekKPF2mSznTQsxqPgnpkI4DNTlrdUmTzrDgektczlKNRRhy5X5AAOnx5S09ydFYWWNSfcEqDTTHgtNA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf-autotable/3.5.28/jspdf.plugin.autotable.min.js" integrity="sha512-03CCNkeosDFN2zCCu4vLpu3pJfZcrL48F3yB8k87ejT+OVMwco7IH3FW02vtbGhdncS6gyYZ/duYaC/K62xQPQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/highcharts/10.3.2/highcharts-3d.min.js" integrity="sha512-kiHRleMJNNgSe7q0AmaYa7CKE8abI5UlvCTIIvk4NtmqQV95rgNYixSqyBFs7AAPVGFgl8NoadDko9Z/9i2LIA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <title>Capgénépi Banque</title>
     <link rel="icon" href="../../files/img/genepi.png">
 </head>
@@ -102,6 +103,8 @@ require_once("../../controller/po_controller.php");
         <section id="comptes_clients">
             <h1><span id="search_result">8</span> Résultat(s) recherche </h1>
             <div id="liste_clients">
+
+                <!-- GENERER ICI LES DONNES SELON LA RECHERCHE -->
                 <div class="client">
                     <div class="client_header">
                         <span class="client_nom">ACTION CONTRE LA SOIF</span>
@@ -166,6 +169,7 @@ require_once("../../controller/po_controller.php");
 
                     </thead>
                     <tbody>
+                        <!-- GENERER ICI EN AJAX LES DONNES SELON LA RECHERCHE -->
                         <tr>
                             <td>2057EJS65</td>
                             <td>Association Action contre la soif</td>
@@ -220,8 +224,49 @@ require_once("../../controller/po_controller.php");
                             <td>01/01/2020</td>
                             <td>1</td>
                             <td>10</td>
-                            <td>100</td>
+                            <td>1000€</td>
                         </tr>
+                        <tr>
+                            <td>2057EJS65</td>
+                            <td>Association Action contre la soif</td>
+                            <td>01/01/2020</td>
+                            <td>1</td>
+                            <td>10</td>
+                            <td>1000€</td>
+                        </tr>
+                        <tr>
+                            <td>2057EJS65</td>
+                            <td>Association Action contre la soif</td>
+                            <td>01/01/2020</td>
+                            <td>1</td>
+                            <td>10</td>
+                            <td>1000€</td>
+                        </tr>
+                        <tr>
+                            <td>2057EJS65</td>
+                            <td>Association Action contre la soif</td>
+                            <td>01/01/2020</td>
+                            <td>1</td>
+                            <td>10</td>
+                            <td>1000€</td>
+                        </tr>
+                        <tr>
+                            <td>2057EJS65</td>
+                            <td>Association Action contre la soif</td>
+                            <td>01/01/2020</td>
+                            <td>1</td>
+                            <td>10</td>
+                            <td>1000€</td>
+                        </tr>
+                        <tr>
+                            <td>2057EJS65</td>
+                            <td>Association Action contre la soif</td>
+                            <td>01/01/2020</td>
+                            <td>1</td>
+                            <td>10</td>
+                            <td>1000€</td>
+                        </tr>
+
 
                 </table>
 
@@ -242,6 +287,7 @@ require_once("../../controller/po_controller.php");
 
                     </thead>
                     <tbody>
+                        <!-- GENERER ICI EN AJAX LES DONNES SELON LA RECHERCHE -->
                         <tr>
                             <td>2057EJS65</td>
                             <td>Association Action contre la soif</td>
@@ -330,7 +376,14 @@ require_once("../../controller/po_controller.php");
 
         </section>
         <section id=" graphique">
-            <h1>graphique</h1>
+            <h1>Graphique</h1>
+            <!-- highcharts graph -->
+            <div id="tresorerie_graph" class="graph" style=" display:block;margin: 0 auto"></div>
+            <div id="remises_graph" class="graph" style=" display:none;margin: 0 auto"></div>
+            <div id="impayes_graph" class="graph" style=" display:none;margin: 0 auto"></div>
+
+
+
 
 
 
@@ -378,10 +431,19 @@ require_once("../../controller/po_controller.php");
                 // add data-selected="true" to this link
                 $(this).attr('data-selected', 'true');
 
+                //disable all graphes 
+                $('.graph').css('display', 'none');
+
+
                 // name of form_link 
                 var name = $(this).attr('name');
                 // name of the form to display
                 var form_name = ".tableau_" + name;
+
+                var graph_name = "#" + name + "_graph";
+                console.log(graph_name);
+                $(graph_name).css('display', 'block');
+
 
                 $(form_name).css('display', 'block');
             });
@@ -412,26 +474,202 @@ require_once("../../controller/po_controller.php");
                     html: table,
                 });
                 doc.save('table.pdf');
-            } else if (value == 'csv') {
-
-
-                var table = "#" + $('.tableau:visible').attr('id') + '_html';
-                var htmltable = document.querySelector(table);
-                var html = htmltable.outerHTML;
-                window.open('data:application/vnd.ms-excel,' + encodeURIComponent(html));
-
-
-            } else if (value == 'xls') {
-                // export to xls with TableExport
-                var table = $('.tableau:visible').attr('id');
-                console.log(table);
-                $('#' + table).tableExport({
-                    type: 'xls',
-                    escape: 'false',
-                    fileName: 'table'
-                });
-
             }
+        });
+    </script>
+
+    <script defer>
+        // GRAPH pour la trésorerie
+
+        // highcharts graph
+        Highcharts.chart('tresorerie_graph', {
+            chart: {
+                type: 'column'
+            },
+            title: {
+                text: 'Tresorerie'
+            },
+            subtitle: {
+                text: 'Source: <a href="https://www.data.gouv.fr/fr/datasets/finances-publiques-departementales/">data.gouv.fr</a>'
+            },
+            xAxis: {
+                // ici chopper les noms des clients 
+                categories: [
+                    'PIVOTEAU SARL',
+                    'ACTION CONTRE LA SOIF',
+                    'ACTION CONTRE LE SEXE',
+                    'ACTION CONTRE L\'ALCOOLISME',
+                    'ACTION CONTRE L\'ACTION',
+
+                ],
+                crosshair: true
+            },
+            yAxis: {
+
+                title: {
+                    text: 'Montant (€)'
+                }
+            },
+            tooltip: {
+                headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
+                pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
+                    '<td style="padding:0"><b>{point.y:.1f} €</b></td></tr>',
+                footerFormat: '</table>',
+                shared: true,
+                useHTML: true
+            },
+            exporting: {
+                enabled: true
+            },
+            plotOptions: {
+                column: {
+                    pointPadding: 0.2,
+                    borderWidth: 0
+                }
+            },
+            series: [{
+                // ICI CHOPPER LES TRESORERIES (dans le meme ordre que les noms des clients) logique !!!!
+                name: 'Dépenses',
+                data: [0, 58, -690, 157, 541]
+
+            }]
+        });
+    </script>
+    <script defer>
+        // GRAPH pour les remises
+
+        // highcharts graph
+        Highcharts.chart('remises_graph', {
+            chart: {
+                type: 'line'
+            },
+            title: {
+                text: 'Remises'
+            },
+            subtitle: {
+                text: 'Source: <a href="https://www.data.gouv.fr/fr/datasets/finances-publiques-departementales/">data.gouv.fr</a>'
+            },
+            xAxis: {
+                // ici chopper les mois ou pas si t'as la flemme ou pas
+                categories: [
+                    'Janvier',
+                    'Fevrier',
+                    'Mars',
+                    'Avril',
+                    'Mai',
+                    'Juin',
+                    'Juillet',
+                    'Aout',
+                    'Septembre',
+                    'Octobre',
+                    'Novembre',
+                    'Decembre'
+                ],
+                crosshair: true
+            },
+            yAxis: {
+                min: 0,
+                title: {
+                    text: 'Montant (€)'
+                }
+            },
+            tooltip: {
+                headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
+                pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
+                    '<td style="padding:0"><b>{point.y:.1f} €</b></td></tr>',
+                footerFormat: '</table>',
+                shared: true,
+                useHTML: true
+            },
+            plotOptions: {
+                column: {
+                    pointPadding: 0.2,
+                    borderWidth: 0
+                }
+            },
+            exporting: {
+                buttons: {
+                    contextButton: {
+                        menuItems: ["downloadPNG", "downloadJPEG", "downloadPDF", "downloadSVG", "separator", "downloadCSV", "downloadXLS", "viewData", "openInCloud"]
+                    }
+                }
+            },
+            series: [{
+                // ICI CHOPPER LES NOMBRE DE REMISES PAR MOIS ( selon la recherche , client ,date etc )
+                name: 'Dépenses',
+                data: [49.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4]
+
+            }]
+        });
+    </script>
+    <script defer>
+        // GRAPH camembert pour les impayes par libelle d'impaye
+
+        // highcharts graph
+
+        Highcharts.chart('impayes_graph', {
+            chart: {
+                plotBackgroundColor: null,
+                plotBorderWidth: null,
+                plotShadow: false,
+                type: 'pie'
+            },
+            title: {
+                text: 'Répartition des motifs d\'impayés'
+            },
+            subtitle: {
+                text: 'Source: <a href="https://www.data.gouv.fr/fr/datasets/finances-publiques-departementales/">data.gouv.fr</a>'
+            },
+            tooltip: {
+                pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+            },
+            plotOptions: {
+                pie: {
+                    allowPointSelect: true,
+                    cursor: 'pointer',
+                    dataLabels: {
+                        enabled: true,
+                        format: '<b>{point.name}</b>: {point.percentage:.1f} %',
+                        style: {
+                            color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
+                        }
+                    }
+                }
+            },
+            exporting: {
+                buttons: {
+                    contextButton: {
+                        menuItems: ["downloadPNG", "downloadJPEG", "downloadPDF", "downloadSVG", "separator", "downloadCSV", "downloadXLS", "viewData", "openInCloud"]
+                    }
+                }
+            },
+            series: [{
+                name: 'Impayes',
+                colorByPoint: true,
+                data: [{
+
+                    // BOUCLER ET CHOPPER LE LIBELLE ET LE NOMBRE D'IMPAYES PAR LIBELLE 
+                    name: 'MEURTRE DU PROPRIETAIRE',
+                    y: 56.33
+                }, {
+                    name: '-180 balles sur le compte (SOFIANE)',
+                    y: 24.03,
+                    sliced: true,
+                    selected: true
+                }, {
+                    name: 'EPICERIE SOLIDAIRE (YANIS)',
+                    y: 10.38
+                }, {
+                    name: '1100 EUROS (DENIS)',
+                    y: 4.77
+                }, {
+                    name: '-900 EUROS DU CROUS (Léon)',
+                    y: 0.91
+                }, {
+                    name: 'Impayes 6',
+                    y: 0.2
+                }]
+            }]
         });
     </script>
 
