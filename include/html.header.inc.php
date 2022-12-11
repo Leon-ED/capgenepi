6fa51ca6ac55e261cf618bcc10e35edf4d157412
Leon-ED <edmeeleon@outlook.fr>
2022-12-11 12:35:57 +0100

    désactivation du cache pour le css

--- include/html.header.inc.php
+++ include/html.header.inc.php
@@ -11,7 +11,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     <script src="https://code.highcharts.com/highcharts.js"></script>
-    <link rel="stylesheet" href="../../files/style.css">
+    <link rel="stylesheet" href="../../files/style.css?cache=<?= time() ?>">
     <script src="https://code.jquery.com/jquery-3.6.1.min.js" integrity="sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ=" crossorigin="anonymous"></script>
     <script src="../../files/product_owner.js" defer></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js" integrity="sha512-qZvrmS2ekKPF2mSznTQsxqPgnpkI4DNTlrdUmTzrDgektczlKNRRhy5X5AAOnx5S09ydFYWWNSfcEqDTTHgtNA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
@@ -19,5 +19,5 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/highcharts/10.3.2/highcharts-3d.min.js" integrity="sha512-kiHRleMJNNgSe7q0AmaYa7CKE8abI5UlvCTIIvk4NtmqQV95rgNYixSqyBFs7AAPVGFgl8NoadDko9Z/9i2LIA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <title>Capgénépi Banque</title>
     <link rel="icon" href="../../files/img/genepi.png">
-    <script src= "../../controller/functions.js"></script>
+    <script src="../../controller/functions.js"></script>
 </head>
