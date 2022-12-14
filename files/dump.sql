64d4127b3363f3958e1340b117c71ec6ebda1bef
Leon-ED <edmeeleon@outlook.fr>
2022-12-14 16:08:28 +0100

    Dump

--- files/dump.sql
+++ files/dump.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Client :  sqletud.u-pem.fr
--- Généré le :  Sam 10 Décembre 2022 à 11:37
+-- Généré le :  Mer 14 Décembre 2022 à 16:07
 -- Version du serveur :  5.7.30-log
 -- Version de PHP :  7.0.33-0+deb9u7
 
@@ -16,9 +16,7 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
---
--- Base de données :  `leon.edmee_db`
---
+
 
 -- --------------------------------------------------------
 
@@ -33,7 +31,7 @@ CREATE TABLE `b__compte` (
   `email` varchar(50) NOT NULL,
   `nom` varchar(50) NOT NULL,
   `prenom` varchar(50) NOT NULL,
-  `role` varchar(50) NOT NULL
+  `role` varchar(50) DEFAULT 'CLIENT'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
@@ -44,7 +42,27 @@ INSERT INTO `b__compte` (`id`, `login`, `password`, `email`, `nom`, `prenom`, `r
 (1, '2001458436', '$2y$10$7xldYUDD6EVmjAPjg/UzBOYl/cjRSoYckiHP5ji7o0aojp/9EKNJG', 'admin@bank.eu', 'Admin', 'nistrateur', 'ADMIN'),
 (2, '4526452419', '$2y$10$9y16F6Qmt5.uR8MY8k2jNOTOhw77AdyRzL9zWs3Qj1dbBNRyYUGM2', 'PO@cap-gemi.ni', 'Product', 'Owner', 'PO'),
 (3, '8755269857', '$2y$10$mcEyY9CCt0US1mOwrxXFAOCjr8AU7JmLLlaVbZgD3NbBMFQ.PVzm.', 'pastille-vomitive@mc.do', 'Ronald', 'McDonald', 'CLIENT'),
-(4, '7745511214', '$2y$10$3iH9qViwGHBjr8V.QnATsOEIWaivRukzVOjvaypvSzf00FUp/r3Lq', 'leroy-sane@arnaq.ue', 'Leroy', 'Merlin', 'CLIENT');
+(4, '7745511214', '$2y$10$3iH9qViwGHBjr8V.QnATsOEIWaivRukzVOjvaypvSzf00FUp/r3Lq', 'leroy-sane@arnaq.ue', 'Leroy', 'Merlin', 'CLIENT'),
+(9, '123456789', '$2y$10$Oam.y9mB735mO2u8byzuWOBrKJ3vkqCGjFBaRodC915waTSVedPLa', 'eee@ee.fr', '123456789', '123456789', 'CLIENT'),
+(10, '789456123', '$2y$10$yl.4Bi1DAVhNO6ZN8lNqgO/d4PV3/XCQDe1GfLRD/Uo9nCEAUfs/a', 'ee.aaa@ee.fr', '789456123', '789456123', 'CLIENT'),
+(11, 'eeeeeeeee', '$2y$10$UbgMmvRi0a9tZjiKMTs.0u6NSQMkB9cnY2NqlwBd1hlMtkJo1wDzi', 'eee@a.fr', 'eeeeeeeee', 'eeeeeeeee', 'CLIENT'),
+(13, 'asxcderfg', '$2y$10$lWsMuwmXGvXF2jXSp9kJnexHqz0OVpEyKJy7CJTAWWbSO07/3f8qK', 'a@b.c', 'asxcderfg', 'asxcderfg', 'CLIENT'),
+(14, '/////////', '$2y$10$At3sfmIMmoSYYpbCM.5WW.fAt/3MhOrYFAzwnqb6ljDC3rIYDzcX6', 'r@r.r', '/////////', '/////////', 'CLIENT'),
+(15, '////////p', '$2y$10$DDg.SL390JeXiOzpNQZPjOXKKqJAJWrm1i.6N0zcKNYdReOnl1/Y2', '////////p', '////////p', '////////p', 'CLIENT'),
+(16, 'ACTION_LOGEMENT', '$2y$10$Fd72.5uKyzQ2QY..qzGiYeffqrkJmp.EChD04erwkhWR/MeCc48.q', 'contact@actionlogement.org', 'ACTION', 'LOGEMENT', 'CLIENT'),
+(17, 'ACTION_LOGEMENTS', '$2y$10$NqIKWsXrqVwLD2sUPeSYU.PePKNhKq.uvU3Y.pME45pxExpL3fuN2', 'contact@actionlogement.orge', 'ACTION', 'LOGEMENT', 'CLIENT'),
+(18, 'ACTION_LOGEMENTSS', '$2y$10$t1/93o6UYvL3RPl7hY7cc.PyavHB9KjLANLQE0486/rp7TEg5KitC', 'conEtact@actionlogement.orge', 'ACTION', 'LOGEMENT', 'CLIENT'),
+(19, '789987789', '$2y$10$wJn5Xu3eFx3OliByMSXcHeL/iZuhsAhcV5o3F7g4baZNwVekPWKxW', '789987789@ee.fr', '789987789', '789987789', 'CLIENT'),
+(20, '789987781', '$2y$10$Itm3Yi7mR8aGuyzKyQk5o.8COv6kPvBcgueLuujlQL1Uk8eKXaK.G', '789987789@ee.fre', '789987789', '789987789', 'CLIENT'),
+(21, '789987777', '$2y$10$vEeJ1g38yxlQHOqwepWYvOY157uJaORX9b41lyRQz0WvgHYE5rix6', 'ee.ee@78.fr', '789987777', '789987777', 'CLIENT'),
+(22, '789987774', '$2y$10$sSMlA41RWrxJSl8r74RxIub/qlZrfaieChvI1d9kvDfa5.NPVJOiC', 'ee.ee@78.fre', '789987774', '789987774', 'CLIENT'),
+(23, '189987774', '$2y$10$tnng50EIbpQQqnKfLNg1E.uT.7J2KKeZ8AQ7YjXvAX6g1nA9OBcaO', 'ee.ee@78.free', '189987774', '189987774', 'CLIENT'),
+(24, '            $(\"form\").trigger(\"reset\");', '$2y$10$EMxmxHuQNScpgp1KlWjeYeW3pxLga.ruTVxqvoGD6jcGwp/21Tv8.', 'e@ee.fr', '            $(\"form\").trigger(\"reset\");', '            $(\"form\").trigger(\"reset\");', 'CLIENT'),
+(25, 'tonton', '$2y$10$o48rqynC/0ozEgywIildbuO9c32EIHKVVz6r4pN5R4ZIggO0qM6fO', 'tonton@ee.fr', 'tonton', 'tonton', 'CLIENT'),
+(26, 'tata', '$2y$10$VGzNUEiHJJYXRAAISR7sPe8m8u5XoPe8c/YhVeJyQNI33vmKegK2C', 'tata@ee.fr', 'tata', 'tata', 'CLIENT'),
+(27, 'tatat', '$2y$10$udMdxsZvM3ssgMNSuPmTeOJqKtA8ZGufSMsviBFWaUWuJEg8NiZ4W', 'tatat@pooe.fr', 'tatat', 'tatat', 'CLIENT'),
+(28, 'tatatp', '$2y$10$l.eqcD3undI9.L91O2z4JOkkxpqgLZgUBITq7XnhM1kqY0Ogxtb06', 'tatatp@xn--eeee-uv7a.fr', 'tatatp', 'tatatp', 'CLIENT'),
+(29, '987', '$2y$10$D8VHAk1GSkDnl7sLWWUKzOAahvNbTbhYFnH2lAFJzgXWalahik8AW', '987@ae.fr', '987', '987', 'CLIENT');
 
 -- --------------------------------------------------------
 
@@ -57,6 +75,31 @@ CREATE TABLE `b__controle` (
   `SIREN` char(9) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Contenu de la table `b__controle`
+--
+
+INSERT INTO `b__controle` (`id`, `SIREN`) VALUES
+(14, '/////////'),
+(15, '////////p'),
+(9, '123456789'),
+(23, '189987774'),
+(10, '789456123'),
+(22, '789987774'),
+(21, '789987777'),
+(20, '789987781'),
+(19, '789987789'),
+(18, '824541144'),
+(17, '824541147'),
+(16, '824541148'),
+(29, '987'),
+(13, 'asxcderfg'),
+(11, 'eeeeeeeee'),
+(26, 'tata'),
+(27, 'tatat'),
+(28, 'tatatp'),
+(25, 'tonton');
+
 -- --------------------------------------------------------
 
 --
@@ -65,7 +108,7 @@ CREATE TABLE `b__controle` (
 
 CREATE TABLE `b__entreprise` (
   `SIREN` char(9) NOT NULL,
-  `Raison_sociale` char(20) NOT NULL
+  `Raison_sociale` char(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
@@ -73,8 +116,28 @@ CREATE TABLE `b__entreprise` (
 --
 
 INSERT INTO `b__entreprise` (`SIREN`, `Raison_sociale`) VALUES
+('/////////', '/////////'),
+('////////p', '////////p'),
 ('120027016', 'Carrefour Banque'),
-('120027017', 'ACTION');
+('120027017', 'ACTION'),
+('123456789', '123456789'),
+('189987774', '189987774'),
+('784587458', 'INACTION CONTRE LA FAIM'),
+('789456123', '789456123'),
+('789987774', '789987774'),
+('789987777', '789987777'),
+('789987781', '789987789'),
+('789987789', '789987789'),
+('824541144', 'ACTION LOGEMENT'),
+('824541147', 'ACTION LOGEMENT'),
+('824541148', 'ACTION LOGEMENT'),
+('987', '987'),
+('asxcderfg', 'asxcderfg'),
+('eeeeeeeee', 'eeeeeeeee'),
+('tata', 'tata'),
+('tatat', 'tatat'),
+('tatatp', 'tatatp'),
+('tonton', 'tonton');
 
 -- --------------------------------------------------------
 
@@ -83,9 +146,16 @@ INSERT INTO `b__entreprise` (`SIREN`, `Raison_sociale`) VALUES
 --
 
 CREATE TABLE `b__impaye` (
-  `numero_dossier_impaye` varchar(50) NOT NULL,
+  `numero_dossier_impaye` int(11) NOT NULL,
   `code` varchar(2) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Contenu de la table `b__impaye`
+--
+
+INSERT INTO `b__impaye` (`numero_dossier_impaye`, `code`) VALUES
+(1, '01');
 
 -- --------------------------------------------------------
 
@@ -97,6 +167,13 @@ CREATE TABLE `b__motifs_impayes` (
   `code` varchar(2) NOT NULL,
   `libelle` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Contenu de la table `b__motifs_impayes`
+--
+
+INSERT INTO `b__motifs_impayes` (`code`, `libelle`) VALUES
+('01', 'Fraude à la carte');
 
 -- --------------------------------------------------------
 
@@ -117,7 +194,9 @@ CREATE TABLE `b__remise` (
 
 INSERT INTO `b__remise` (`id`, `SIREN`, `date_traitement`, `devise`) VALUES
 (1, '120027016', '2022-12-10', 'EUR'),
-(2, '120027017', '2022-12-08', 'USD');
+(2, '120027017', '2022-12-08', 'USD'),
+(3, '120027016', '2022-12-10', 'EUR'),
+(4, '120027017', '2022-12-08', 'USD');
 
 -- --------------------------------------------------------
 
@@ -138,31 +217,6 @@ INSERT INTO `b__role` (`role`, `libelle`) VALUES
 ('ADMIN', 'Administrateur'),
 ('CLIENT', 'Client'),
 ('PO', 'Product Owner');
-
--- --------------------------------------------------------
-
---
--- Structure de la table `b__transaction`
---
-
-CREATE TABLE `b__transaction` (
-  `numero_transaction` int(11) NOT NULL,
-  `montant` int(11) NOT NULL,
-  `date_transaction` date NOT NULL,
-  `sens` char(1) NOT NULL,
-  `numero_carte` char(16) NOT NULL,
-  `numero_dossier_impaye` varchar(50) DEFAULT NULL,
-  `SIREN` char(9) NOT NULL,
-  `id_remise` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Contenu de la table `b__transaction`
---
-
-INSERT INTO `b__transaction` (`numero_transaction`, `montant`, `date_transaction`, `sens`, `numero_carte`, `numero_dossier_impaye`, `SIREN`, `id_remise`) VALUES
-(1, 581, '2022-12-09', '+', '134543', NULL, '120027016', 1),
-(2, -658, '2022-12-09', '-', 'EZE', NULL, '120027017', 2);
 
 --
 -- Index pour les tables exportées
@@ -219,15 +273,6 @@ ALTER TABLE `b__role`
   ADD UNIQUE KEY `libelle` (`libelle`);
 
 --
--- Index pour la table `b__transaction`
---
-ALTER TABLE `b__transaction`
-  ADD PRIMARY KEY (`numero_transaction`),
-  ADD KEY `numero_dossier_impaye` (`numero_dossier_impaye`),
-  ADD KEY `SIREN` (`SIREN`),
-  ADD KEY `id_remise` (`id_remise`);
-
---
 -- AUTO_INCREMENT pour les tables exportées
 --
 
@@ -235,17 +280,17 @@ ALTER TABLE `b__transaction`
 -- AUTO_INCREMENT pour la table `b__compte`
 --
 ALTER TABLE `b__compte`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
+--
+-- AUTO_INCREMENT pour la table `b__impaye`
+--
+ALTER TABLE `b__impaye`
+  MODIFY `numero_dossier_impaye` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 --
 -- AUTO_INCREMENT pour la table `b__remise`
 --
 ALTER TABLE `b__remise`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
---
--- AUTO_INCREMENT pour la table `b__transaction`
---
-ALTER TABLE `b__transaction`
-  MODIFY `numero_transaction` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 --
 -- Contraintes pour les tables exportées
 --
@@ -274,13 +319,6 @@ ALTER TABLE `b__impaye`
 --
 ALTER TABLE `b__remise`
   ADD CONSTRAINT `b__remise_ibfk_1` FOREIGN KEY (`SIREN`) REFERENCES `b__entreprise` (`SIREN`);
-
---
--- Contraintes pour la table `b__transaction`
---
-ALTER TABLE `b__transaction`
-  ADD CONSTRAINT `b__transaction_ibfk_1` FOREIGN KEY (`numero_dossier_impaye`) REFERENCES `b__impaye` (`numero_dossier_impaye`),
-  ADD CONSTRAINT `b__transaction_ibfk_2` FOREIGN KEY (`id_remise`) REFERENCES `b__remise` (`id`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
